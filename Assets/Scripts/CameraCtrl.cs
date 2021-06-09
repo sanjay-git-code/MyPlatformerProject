f14d7e3fcb6c32624e656e132fa556a458195389
John Doe <johndoe@example.com>
2021-06-09 19:40:23 +0530

    Added Camera Script to follow player

---
+++ Assets/Scripts/CameraCtrl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Makes the camera follow the player
+/// </summary>
+public class CameraCtrl : MonoBehaviour
+{
+    public Transform player;
+    public float offset;
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        //makes the camera follow the player in y axis
+        transform.position = new Vector3(player.position.x, player.position.y + offset, transform.position.z);
+
+    }
+}
